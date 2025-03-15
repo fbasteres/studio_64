f4ef37486046d4e4a90981f5f7197a282c2805c8
EpsilonX-DEV <epsilonx.enterprise@gmail.com>
2025-03-15 18:28:07 -0500

    Produccion de Servicios

--- resources/views/pages/components/servicios.blade.php
+++ resources/views/pages/components/servicios.blade.php
@@ -3,13 +3,13 @@
     <div class="container xl:max-w-6xl mx-auto px-4">
         <!-- Heading start -->
         <header class="text-center mx-auto mb-12 lg:px-20">
-            <h2 class="text-2xl leading-normal mb-2 font-bold text-black">What We Do</h2>
+            <h2 class="text-2xl leading-normal mb-2 font-bold text-black">Más Servicios</h2>
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 60" style="margin: 0 auto;height: 35px;" xml:space="preserve">
                 <circle cx="50.1" cy="30.4" r="5" class="stroke-primary" style="fill: transparent;stroke-width: 2;stroke-miterlimit: 10;"></circle>
                 <line x1="55.1" y1="30.4" x2="100" y2="30.4" class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
                 <line x1="45.1" y1="30.4" x2="0" y2="30.4" class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
             </svg>
-            <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">Save time managing advertising &amp; Content for your business.</p>
+            <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">Entérate de más servicios &amp; promociones que tenemos para tí.</p>
         </header>
         <!-- End heading -->
         <!-- row -->
@@ -23,8 +23,8 @@
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
                         </svg>
                     </div>
-                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">SEO Services</h3>
-                    <p class="text-gray-500">This is a wider card with supporting text below as a natural content.</p>
+                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Grabaciones con DRONE.</h3>
+                    <p class="text-gray-500">Lleva tu producción al siguiente nivel con nuestras grabaciones aéreas en alta definición. Gracias a la tecnología de drones de última generación.</p>
                 </div>
                 <!-- end service block -->
             </div>
@@ -38,8 +38,8 @@
                             <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"></path>
                         </svg>
                     </div>
-                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Social Content</h3>
-                    <p class="text-gray-500">This is a wider card with supporting text below as a natural content.</p>
+                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Photobook.</h3>
+                    <p class="text-gray-500">Preserva tus recuerdos más especiales con un fotolibro profesional, diseñado para capturar cada uno.</p>
                 </div>
                 <!-- end service block -->
             </div>
@@ -53,8 +53,8 @@
                             <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"></path>
                         </svg>
                     </div>
-                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Creative ads</h3>
-                    <p class="text-gray-500">This is a wider card with supporting text below as a natural content.</p>
+                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Grabaciones en USB personalizado.</h3>
+                    <p class="text-gray-500">Guarda tus recuerdos más valiosos en un USB personalizado, ideal para conservar y revivir cada momento especial con calidad profesional.</p>
                 </div>
                 <!-- end service block -->
             </div>
@@ -68,8 +68,8 @@
                             <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"></path>
                         </svg>
                     </div>
-                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Brand Identity</h3>
-                    <p class="text-gray-500">This is a wider card with supporting text below as a natural content.</p>
+                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Cuadros personalizados.</h3>
+                    <p class="text-gray-500">Convierte tus momentos favoritos en obras de arte con nuestros cuadros personalizados de alta calidad. Perfectos para decorar tu hogar.</p>
                 </div>
                 <!-- end service block -->
             </div>
@@ -78,12 +78,13 @@
                 <div class="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
                     <div class="inline-block text-gray-900 mb-4">
                         <!-- icon -->
-                        <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" class="bi bi-wallet2" viewBox="0 0 16 16">
-                            <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"></path>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
                         </svg>
+
                     </div>
-                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Budget &amp; Marketing</h3>
-                    <p class="text-gray-500">This is a wider card with supporting text below as a natural content.</p>
+                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Cuadros &amp; Video Collage de Fotos.</h3>
+                    <p class="text-gray-500">Transforma tus fotos favoritas en un video emocionante y lleno de vida. Nuestros videos collage de fotos son la manera perfecta de revivir momentos especiales, contar una historia o promocionar un evento.</p>
                 </div>
                 <!-- end service block -->
             </div>
@@ -96,8 +97,8 @@
                             <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"></path>
                         </svg>
                     </div>
-                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Optimize conversions</h3>
-                    <p class="text-gray-500">This is a wider card with supporting text below as a natural content.</p>
+                    <h3 class="text-lg leading-normal mb-2 font-semibold text-black">Video Invitación.  </h3>
+                    <p class="text-gray-500">¿Quieres que tu evento sea inolvidable desde el primer momento? Con nuestros videos de invitación personalizados, transformamos tu mensaje en una experiencia visual única y emocionante. Ideal para tus eventos y más.</p>
                 </div>
                 <!-- end service block -->
             </div>
@@ -105,3 +106,14 @@
         <!-- end row -->
     </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
