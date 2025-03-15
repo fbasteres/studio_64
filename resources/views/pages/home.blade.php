4a63fb968569a6808fd34671792b323c983fbd31
fbasteres <ferjav4351@gmail.com>
2025-03-15 17:13:08 -0500

    NUeva sesion de servicios

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 @include('pages.components.iniciovideo')
 @include('pages.components.destino')
+@include('pages.components.servicios')
 
 @include('pages.components.whatsapp_flotante')
 @endsection
