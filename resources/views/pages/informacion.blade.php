18a5199d62efc3f73ae8fb79ff0094fc0f5b1c2e
EpsilonX-DEV <epsilonx.enterprise@gmail.com>
2025-03-16 14:03:05 -0500

    Alineacion informacion

--- resources/views/pages/informacion.blade.php
+++ resources/views/pages/informacion.blade.php
@@ -5,9 +5,9 @@
 <section
   class="overflow-hidden bg-[url(https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?q=80&w=2670&auto=format&fit=crop)] bg-cover bg-top bg-no-repeat"
 >
-<br>
+<br></br>
   <div class="bg-black/50 p-8 md:p-12 lg:px-16 lg:py-24">
-    <div class="container text-left">
+    <div class="text-rigth ltr:sm:text-left rtl:sm:text-right container px-4">
       <h2 class="text-2xl font-bold text-white sm:text-3xl md:text-5xl">Información</h2>
         <p class="hidden max-w-lg text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed"></p>
     </div>
